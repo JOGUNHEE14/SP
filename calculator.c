e9429a0a183d89f54147f9b2719b0d94c172a7cb
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-11-21 13:40:19 +0900

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -16,8 +16,14 @@ int priority(struct stackNode *target, char com) {
         char result = (target -> next) -> data;
         if (result =='*')
                 result += 5;
+        else if (result == '+')
+                result += 2;
+        
         if (com == '*')
                 com += 5;
+        else if (result == '+')
+                result += 2;
+        
         if (result < com)
                 return 2;
         else if (result == com)
