acc811a0886a066be8a69a7d37d6e7dfeda18319
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-11-21 14:17:54 +0900

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//연산자 넣을 노드
 struct stackNode {
         struct stackNode *next;
         char data;
 };
 
+//숫자 넣을 노드
 struct listNode {
         struct listNode *previous;
         struct listNode *next;
         char data;
 };
 
+//연산자 비교하는 코드 -,+를 같은 레벨, *,/ 를 같은 레벨로 만들고 result와 com에 저장된 연산자 비교해서 리턴턴
 int priority(struct stackNode *target, char com) {
         char result = (target -> next) -> data;
         if (result =='*')
@@ -31,6 +34,7 @@ int priority(struct stackNode *target, char com) {
         return 0;
 }
 
+//노드를 추가하는 함수
 void push(struct stackNode *target, char data)
 {
         struct stackNode *pushNode = malloc(sizeof(struct stackNode));
@@ -40,6 +44,7 @@ void push(struct stackNode *target, char data)
         target -> next = pushNode;
 }
 
+//노드에 저장된 데이터를 리턴하고 저장공간 해제
 char pop(struct stackNode *target)
 {
         char popData;
@@ -55,21 +60,23 @@ char pop(struct stackNode *target)
 
 int main(void)
 {
-        char x;
-        int l = 0;
-        struct stackNode *operator = malloc(sizeof(struct stackNode));
+        char x; //입력을 하나하나 받을 때 쓸 문자열 타입변수
+        int l = 0; // 괄호가 스택에 있는지 확인할 때 쓸 변수
+        struct stackNode *operator = malloc(sizeof(struct stackNode)); // 스택을 처음 만들 때 operator를 사용해서 push할 예정정
 
-        operator -> next = NULL;
+        operator -> next = NULL; // operator가 가라키는 포인터 초기화
 
         printf("Input: \n");
 
         while ((x = getchar()) != '\n') {
-                if (x == ' ');
+                if (x == ' '); // 대충 공백처리
 
-                else if ('0' <= x && x <= '9')
+                // 대충 숫자면 프린트 -> 숫자를 리스트노드를 이용해서 저장해야함
+                else if ('0' <= x && x <= '9')  
                         printf("%c ", x);
-
-                else if (x == ')') {
+                
+                // 닫는 괄호가 입력으로 들어오면 여는 괄호가 나올 때 까지 연산자 꺼내기
+                else if (x == ')') {  
                         while (operator -> next != NULL) {
                                char oper = pop(operator);
                                if (oper == '(')
@@ -79,15 +86,18 @@ int main(void)
                         l-=1;
                 }
 
-                else if (x == '(') {
+                // 여는 괄호가 들어오면 l변의 값을 늘림
+                else if (x == '(') { 
                         push(operator, x);
                         l+=1;
                 }
 
+                // 여는 괄호가 주워지면 계속 연산자를 넣어야하기 때문에 l이 0이 아니고 스택의 마지막에 저장된 연산자보다 넣을 연산자가 크면 push
                 else if (l != 0 || operator -> next == NULL || priority(operator, x) == 2) {
                         push(operator, x);
                 }
 
+                // 그 외의 상황에는 스택에 연산자를 가능한만큼 pop하고 연산자 push
                 else {
                         while (operator -> next != NULL) {
                                 if (priority(operator, x) == 2)
