a3e1cb36934cbbfde9d287661c66d8ebdaa596ab
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-11-23 20:21:25 +0900

    Create priotrity.c

---
+++ seperation/priotrity.c
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+int priority(struct stackNode *target, char com) {
+        char result = (target -> next) -> data;
+        if (result =='*')
+                result += 5;
+        else if (result == '+')
+                result += 2;
+        
+        if (com == '*')
+                com += 5;
+        else if (result == '+')
+                result += 2;
+        
+        if (result < com)
+                return 2;
+        else if (result == com)
+                return 1;
+        return 0;
+}
