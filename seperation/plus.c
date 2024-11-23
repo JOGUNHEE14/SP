c1a082750700b5577f824d2fda9e8d89b16fabf1
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-11-23 21:52:25 +0900

    Update plus.c

--- seperation/plus.c
+++ seperation/plus.c
@@ -1,4 +1,42 @@
 #include "calculator.h"
 
+void plus(struct listNode *front, struct listNode *rear)
+{
+        struct listNode *head = malloc(sizeof(struct listNode));
+        char alpha = '0';
 
-void plus(struct listNode *front, struct listNode *rear);
+        while (1) {
+                if (front -> previous != NULL && rear -> previous != NULL) {
+                        char x = listrpop(front);
+                        char y = listrpop(rear);
+                }
+                else
+                        break;
+
+
+                listlpush(head,(x+y)%10+48+alpha%48);
+
+                if (x+y-96 >= 10)
+                        alpha = '1';
+                else
+                        alpha = '0';
+        }
+
+        if (alpha == '1')
+                listlpush(head, alpha);
+
+        if (front -> previous == NULL) {
+                free(front -> next);
+                free(front);
+                rear -> next = head;
+                return rear;
+        }
+        else if (rear -> previous == NULL) {
+                free(rear -> next);
+                free(rear);
+                front -> next = head;
+                return front;
+        }
+        else
+                return head;
+}
