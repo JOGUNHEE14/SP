b4eb974538a2504581a8ca1967971fbf38a49a70
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-02 12:38:50 +0900

    Update list.c

--- seperation/list.c
+++ seperation/list.c
@@ -1,1 +1,24 @@
+#include "calculator.c"
 
+void listrpush(struct listNode *target, char data)
+{
+        struct listNode *pushNode = malloc(sizeof(struct listNode));
+        struct listNode *nextNode = target -> next;
+
+        pushNode -> next = nextNode;
+        pushNode -> previous = target;
+        target -> next = pushNode;                                                                                              
+        nextNode -> previous = pushNode;
+}
+
+void listlpush()
+{
+}
+
+char listrpop(struct listNode *target)
+{
+        char popData;
+        struct listNode *popNode = target -> next;
+
+        target -> next = listNode -> next;
+        listNode                                                                                                        ~                
