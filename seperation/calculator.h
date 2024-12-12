6a980aa86165dea8a4e3afb84990b4245da125df
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-12 13:01:12 +0900

    Update calculator.h

--- seperation/calculator.h
+++ seperation/calculator.h
@@ -14,12 +14,13 @@ struct listNode {
         char data;
 };
 
-//숫자를 저장한 list노드를 저장하는 노드
+//숫자를 저장한 info노드를 저장하는 노드
 struct queueNode {
         struct queueNode *next;
         struct inforNode *data;
 };
 
+// infoNode 는 소수점을 포함한 숫자를 저장하기 위해 만든 노드. 자연수의 앞, 뒤 부분과 소수부분의 앞, 뒤부분의 위치를 저장함.
 // 큐에 infroNode를 저장해서 사용할 예정. 자연수는 natural_부분에 소수는 decimal_에 저장하고 부호는 data에 저장예정
 struct inforNode {
         struct listNode *natural_head;
@@ -30,7 +31,6 @@ struct inforNode {
 };
 
 int priority(struct stackNode *target, char com);
-
 void stackpush(struct stackNode *target, char data);
 char stackpop(struct stackNode *target);
 void plus(struct listNode *front, struct listNode *rear);
