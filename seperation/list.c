550f961007581d9dd3aef1e892708facb1f947cb
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-13 14:26:15 +0900

    Update list.c

--- seperation/list.c
+++ seperation/list.c
@@ -1,19 +1,21 @@
 #include "calculator.c"
 
-void listrpush(struct listNode *target, char data)
+void listrpush(struct list *target, char data)
 {
         struct listNode *pushNode = malloc(sizeof(struct listNode));
-
+        
+        pushNode -> next = NULL;
+        pushNode -> previous = target -> tail;
         pushNode-> data = data;
         
-        struct listNode *preNode = target -> previous
-
-        pushNode -> next = target;
-        pushNode -> previous = preNode;
-        target -> previous = pushNode;                                                                                              
-        preNode -> next = pushNode;
+        if (target -> tail != NULL) 
+                target -> tail -> next = pushNode;
+        else
+                target -> head = pushNode;
+        target -> tail = pushNode;
 }
 
+//수정 해야함
 void listlpush(struct listNode *target, char data)
 {
         struct listNode *pushNode = malloc(sizeof(struct listNode));
@@ -29,22 +31,24 @@ void listlpush(struct listNode *target, char data)
         target->next = pushNode;
 }
 
-char listrpop(struct listNode *target)
+
+char listrpop(struct list *target)
 {
-        struct listNode *popNode = target -> previous;
-        struct listNode *preNode = popNode -> previous;
+        struct listNode *popNode = target -> tail;
 
         char popData = popNode-> data;
-        
-        preNode -> next = target;
-        target -> previous = preNode;
-        popData = popNode -> data;
+        popNode -> next = NULL;
+        target -> tail = popNode -> previous;
+
+        if (popNode -> previous = NULL)
+                target -> head = NULL;
         
         free(popNode);            
 
         return popData;
 }
 
+// 수정해야함
 char listlpop(struct listNode *target)
 {
         struct listNode *popNode = target -> next;
