946ed4afead3dad025154c3f3de90052870db3d2
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-13 15:59:34 +0900

    Update inforNode.c

--- seperation/inforNode.c
+++ seperation/inforNode.c
@@ -1,2 +1,18 @@
 #include "calculator.c"
 
+struct inforNode *init() 
+{
+        struct inforNode *number = malloc(sizeof(struct inforNode));
+        number -> natural = malloc(sizeof(struct list));
+        number -> decimal = malloc(sizeof(struct list));
+        number -> data = '+'; 
+
+        natural_number -> head = NULL;
+        natural_number -> tail = NULL;
+        decimal_number -> head = NULL;
+        decimal_number -> tail = NULL;
+
+        return number;
+}
+    
+  
