44cdbec6241cff5002991a5f2cbb011ef7c9c436
terrrrry416 <xoffl2@naver.com>
2024-12-14 23:19:06 +0900

    Update minus.c

--- seperation/minus.c
+++ seperation/minus.c
@@ -1,41 +1,179 @@
 #include "calculator.h"
 
-void minus(struct listNode *front, struct listNode *rear)
-{
-        struct listNode *head = malloc(sizeof(struct listNode));
-        int beta =  0;
+struct listNode* minus(struct inforNode* front, struct inforNode* rear) {
+    struct inforNode *result = init();
 
-        while (1) {
-                if (front -> previous != NULL && rear -> previous != NULL) {
-                        char x = listrpop(front);
-                        char y = listrpop(rear);
-                }
-                else
-                        break;
+    char beta= '0';  // 자릿수빼기
+    char resultt;
+    char x;
+    char y;
+    int com = 0; //front 랑 rear중 뭐가큰지 구분 front일시1 rear일시 2
+    
+    //front 와 rear 값 비교
+    struct listNode* naturalFront = front->natural->head;
+    struct listNode* naturalRear = rear->natural->head;
+    struct listNode* decimalFront = front->decimal->head;
+    struct listNode* decimalRear = rear->decimal->head;
 
-                listlpush(head, (x - beta < y) ? (x - y + 10) : (x - y));  
+    while (1) {
+	if (naturalFront == NULL && naturalRear != NULL) {
+		com = 2;
+		break;
+	}
+	else if (naturalFront != NULL && naturalRear == NULL) {
+		com = 1;
+		break;
+	}
+	else if (naturalFront == NULL && naturalRear == NULL) {
+		com = 3;
+		break;
+	}
+	naturalFront = naturalFront->next;
+	naturalRear = naturalRear->next;
+    }
+		//다시써야되서 초기화
+    naturalFront = front->natural->head;
+    naturalRear = rear->natural->head;
+    if (com == 3) {
+	while (1) {
 
-                if (x - beta < y)
-                        beta = 1;
-                else
-                        beta = 0;
+		if (naturalFront == NULL && naturalRear == NULL) {
+			com = 4;
+			break;
+		}
 
+		x = listlpop(front->natural);
+		y = listlpop(rear->natural);
+
+		if (x > y) {
+			com = 1;
+			break;
+		}
+		else if (y > x) {
+			com = 2;
+			break;
+		}
+		naturalFront = naturalFront->next;
+      	        naturalRear = naturalRear->next;
+	}
+    }
+		//소수점 자릿수 맞추기위해 0채우기
+    while (decimalFront != NULL || decimalRear != NULL) {
+	if (decimalFront == NULL) {
+     		listlpush(front->decimal->head, '0'); // 상위 리스트의 head를 수정
         }
-        if (beta == 1)
-                listlpush(head, beta);
+	if (decimalRear == NULL) {
+    		listlpush(rear->decimal->head, '0'); // 상위 리스트의 head를 수정
+	}
 
-        if (front -> previous == NULL) {
-                free(front -> next);
-                free(front);
-                rear -> next = head;
-                return rear;
-        }
-        else if (rear -> previous == NULL) {
-                free(rear -> next);
-                free(rear);
-                front -> next = head;
-                return front;
-        }
-        else
-                return head;
+	if (decimalFront != NULL) {
+		decimalFront = decimalFront->next;
+	}
+	if (decimalRear != NULL) {
+		decimalRear = decimalRear->next;	
+	}	
+    	
+    }
+	
+    decimalFront = front->decimal->head;
+    decimalRear = rear->decimal->head;
+
+    if (com == 4) {
+	while (decimalFront != NULL && decimalRear != NULL) {
+		x = listlpop(front->decimal);
+		y = listlop(rear->decimal);
+
+		if (x > y) {
+			com = 1;
+			break;
+		}
+		else if (y > x) {
+			com = 2;
+			break;
+		}
+		decimalFront = decimalFront->next;
+      	        decimalRear = decimalRear->next;
+	}
+    }
+	
+    decimalFront = front->decimal->tail;
+    decimalRear = rear->decimal->tail;
+	//뺄셈 시작
+    if (com ==1) {
+	    while (1) {
+		if (decimalFront != NULL && decimalRear != NULL) {
+			 x = listrpop(front->decimal);
+			 y = listrpop(rear->decimal); 
+			 (x - beta%48 < y) ? beta = '1' : '0';
+	
+			
+			resultt = (x - beta%48 < y) ? (x - beta%48 + 10 - y) + 48 : (x - beta%48 - y);
+			listrpush(result ->decimal, resultt);
+		else
+			break;
+		}
+
+   	  // 자연수 부분 계산
+   	  struct listNode* naturalFront = front->natural->tail;
+          struct listNode* naturalRear = rear->natural->tail;
+
+   	  while (1) {
+		if (naturalFront != NULL || naturalRear != NULL) {
+			 x = (naturalFront != NULL) ? listrpop(front->natural) : '0';
+			 y = (naturalRear != NULL) ? listrpop(rear->natural) : '0';
+			 (x - beta%48 < y) ? beta = '1' : '0';
+			
+		 	 resultt = (x - beta%48 < y) ? (x - beta%48 + 10 - y) + 48 : (x - beta%48 - y);
+			 listrpush(result ->natural, resultt);
+			
+		else
+			break;
+		
+         }
+
+    if (com ==2) {
+	    while (1) {
+		if (decimalFront != NULL && decimalRear != NULL) {
+			x = listrpop(front->decimal);
+			y = listrpop(rear->decimal); 
+			(y - beta%48 < x) ? beta = '1' : '0';
+	
+			
+			resultt = (y - beta%48 < x) ? (y - beta%48 + 10 - x) + 48 : (y - beta%48 - x);
+			listrpush(result ->decimal, resultt);
+		else
+			break;
+		}
+
+   	  // 자연수 부분 계산
+   	  struct listNode* naturalFront = front->natural->tail;
+          struct listNode* naturalRear = rear->natural->tail;
+
+   	  while (1) {
+		if (naturalFront != NULL || naturalRear != NULL) {
+			x = (naturalFront != NULL) ? listrpop(front->natural) : '0';
+			y = (naturalRear != NULL) ? listrpop(rear->natural) : '0';
+			(y - beta%48 < x) ? beta = '1' : '0';
+			
+			resultt = (y - beta%48 < x) ? (y - beta%48 + 10 - x) + 48 : (y - beta%48 - x);
+			listrpush(result ->natural, resultt);
+			
+		else
+			break;
+		
+         }
+
+		
+        	
+    free(front -> natural);
+    free(front -> decimal); 
+    free(rear -> natural);
+    free(rear -> decimal);
+    free(front);
+    free(rear);
+	
+    free(front);
+    free(rear);	
+
+    return result;
 }
