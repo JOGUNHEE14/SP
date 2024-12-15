f0ec7f60731487cbb6f9b305d12c83eeb5df770d
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-15 15:18:00 +0900

    Update multiple.c

--- seperation/multiple.c
+++ seperation/multiple.c
@@ -12,6 +12,8 @@ struct inforNode *multiple(struct inforNode *front, struct inforNode *rear)
         int num = 0;
         int alpha = 0;
         int total = 0;
+        int y = 0;
+        int x = 0;
         // 0일 때 빨리 리턴
         if (front -> natural -> head != NULL && front -> natural -> head -> data == '0' && front -> decimal -> head == NULL) {
                 listrpush(result -> natural ,'0');
@@ -29,46 +31,58 @@ struct inforNode *multiple(struct inforNode *front, struct inforNode *rear)
 
         listlpush(result -> natural, '0');
         tmpResult = result -> natural -> tail;
-
+        struct listNode *b;
         while (tmpFront != NULL)
         {
                 alpha = 0;
+                y = tmpFront -> data - '0';
                 moveResult = tmpResult;
+                
                 if (rear -> decimal -> tail != NULL)
                         moveRear = rear -> decimal -> tail;
                 else
                         moveRear = rear -> natural -> tail;
-                printf("-----------\n");
+                
                 while (1) {
-                        printf("%c\n",alpha);
                         num = moveResult -> data - '0';
-                        total = alpha + num + (moveRear -> data * tmpFront -> data)%48;
+                        x = moveRear -> data - '0';
+                        total = alpha + num + x * y;
                         alpha = total/10;
+                        
                         if (moveResult -> previous == NULL)
                                 listlpush(result -> natural, '0');
-                        moveResult -> data = total%10 + 48;
-                        printf("alpah : %d, d : %c\n",alpha, moveResult -> data);
+                        
+                        moveResult -> data = total % 10 + 48;
                         if (moveRear -> previous == NULL)
                                 break;
                         moveResult = moveResult -> previous;
                         moveRear = moveRear -> previous;
 
                 }
-                printf("----------\n");
-                if(moveResult -> previous == NULL)
-                        listlpush(result -> natural, '0');
-                moveResult = moveResult -> previous;
+                b = result -> natual -> head;
+                while (b!=NULL) {
+                        printf("%c",b->data);
+                        b = b -> next;
+                }
                 if (moveRear == rear -> decimal -> head) {
+                        if(moveResult -> previous == NULL)
+                                listlpush(result -> natural, '0');
+                        
+                        moveResult = moveResult -> previous;
                         moveRear = rear -> natural -> tail;
+                        
                         while (1) {
-                                printf("%c\n",alpha);
                                 num = moveResult -> data - '0';
-                                total = alpha + num + (moveRear -> data * tmpFront -> data)%48;
+                                x = moveRear -> data - '0';
+                                total = alpha + num + x * y;
                                 alpha = total/10;
+                                
                                 if (moveResult -> previous == NULL)
                                         listlpush(result -> natural, '0');
-                                moveResult -> data = total%10 + 48;
-                                printf("alpha : %d, d : %c\n",alpha,moveResult -> data);
+                                
+                                moveResult -> data = total % 10 + 48;
+                                
+                        
                                 if (moveRear -> previous == NULL)
                                         break;
                                 moveResult = moveResult -> previous;
@@ -80,7 +94,7 @@ struct inforNode *multiple(struct inforNode *front, struct inforNode *rear)
                                 listlpush(result -> natural, alpha+48);
                         else
                                 moveResult -> previous -> data = alpha+48;
-                        printf("d : %c\n",moveResult->previous->data);
+                        
                 }
                 if (tmpFront -> previous != NULL)
                         tmpFront = tmpFront -> previous;
@@ -92,16 +106,12 @@ struct inforNode *multiple(struct inforNode *front, struct inforNode *rear)
                 tmpResult = tmpResult -> previous;
 
         }
-        char x;
 
         while(rear -> decimal -> head != NULL) {
                 if (result -> natural -> tail == NULL)
                         listrpush(result -> natural,'0');
-                x = listrpop(result -> natural);
-                printf("%c",x);
-                listlpush(result -> decimal,x);
+                listlpush(result -> decimal, listrpop(result -> natural));
                 listrpop(rear -> decimal);
-                result->natural -> tail = result -> decimal -> head -> previous;
         }
         while (rear -> natural -> head != NULL)
                 listrpop(rear -> natural);
@@ -109,11 +119,8 @@ struct inforNode *multiple(struct inforNode *front, struct inforNode *rear)
         while(front -> decimal -> head != NULL) {
                 if (result -> natural -> tail == NULL)
                         listrpush(result -> natural,'0');
-                x = listrpop(result -> natural);
-                printf("%c",x);
-                listlpush(result -> decimal,x);
+                listlpush(result -> decimal, listrpop(result -> natural));
                 listrpop(front -> decimal);
-                result -> natural -> tail = result -> decimal -> head -> previous;
         }
         while (front -> natural -> head != NULL)
                 listrpop(front -> natural);
