71f4c5d6379e019e2d45e9229f16eb885478baa8
ObedientManx700 <yeonjunjeong0@gmail.com>
2024-12-18 17:11:31 +0900

    Update list.c //write annotation

--- seperation/list.c
+++ seperation/list.c
@@ -11,27 +11,27 @@ void listrpush(struct list *target, char data) //리스트 tail에 데이터 추
         pushNode -> previous = target -> tail; // 새 노드의 previous를 tail로 설정
         pushNode-> data = data; // 새 노드에 전달된 데이터 저장
 
-        if (target -> tail != NULL) // 리스트에 원래 tail이 있다면, tail의 next를 새 노드로 연결
+        if (target -> tail != NULL) // 리스트에 원래 tail이 있다면 tail의 next를 새 노드로 연결
                 target -> tail -> next = pushNode;
         else //없다면 head를 새노드로 설정
                 target -> head = pushNode;
         target -> tail = pushNode; // tail을 새 노드로 갱신
 }
 
-void listlpush(struct list *target, char data) // 리스트 head에 테이터를 추가하는 함수
+void listlpush(struct list *target, char data) // 리스트 head에 데이터를 추가하는 함수
 {
-        struct listNode *pushNode = malloc(sizeof(struct listNode));
+        struct listNode *pushNode = malloc(sizeof(struct listNode)); // 메모리 동적 할당.
 
-        pushNode->next = target->head; 
-        pushNode->previous = NULL;
-        pushNode->data = data;
+        pushNode->next = target->head; //새노드의 next를 head로 설정
+        pushNode->previous = NULL; // 새 노드의 previous를 NULL로 설정
+        pushNode->data = data; //노드에 데이터 저장
 
-        if (target->head != NULL)
+        if (target->head != NULL) //리스트에 원래 head가 있으면 head의 previous를 새 노드로 연결
                 target->head->previous = pushNode;
-        else
-                target->tail = pushNode;
+        else // 없다면 tail을 새 노드로 설정.
+                target->tail = pushNode; 
 
-        target->head = pushNode;
+        target->head = pushNode; // head를 새 노드로 갱신
 }
 char listrpop(struct list *target) // tail에서 데이터를 제거하고 pop하는 함수
 {
