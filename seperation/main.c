bcad4d4c9771828128ff158168bba0862d3f2194
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-19 10:07:27 +0900

    Update main.c

--- seperation/main.c
+++ seperation/main.c
@@ -3,10 +3,8 @@
 int main(void)
 {
         char pre = '\0'; // x(현재)의 이전 문자
-        char x; //입력을 하나하나 받을 때 쓸 문자열 타입변수
-        int gwalho = 0; // 괄호가 스택에 있는지 확인할 때 쓸 변수
-        int start = 1;
-        int minus = 0;
+        char x = '\0'; //입력을 하나하나 받을 때 쓸 문자열 타입변수
+        int sign = 0;
         struct stackNode *operator = malloc(sizeof(struct stackNode)); // 스택을 처음 만들 때 operator를 사용해서 push할 예정
         struct queueNode *numbers = malloc(sizeof(struct queueNode)); // listNode에 저장된 숫자와 부호가 저장된 infoNode를 저장할 예정 
         struct inforNode *front_number;
@@ -27,14 +25,16 @@ int main(void)
 
         while (x!= EOF && x != '\n') {
                 x=getc(file);
-                pre = x; //이전 문자(pre) 업데이트
                 
                 if (x == ' '); // 대충 공백처리
 
                 // 대충 숫자면 프린트 -> 숫자를 inforNode를 이용해서 저장해야함
                 else if (('0' <= x && x <= '9')) {
                         struct inforNode *number = init();
-                        
+                        if (pre == '-') {
+                                number -> data = '-';
+                        }
+                        pre = x;
                         //data에 부호 넣어야 하는데 대충 플러스로 함
                         while ('0' <= x && x <= '9') {
                                 listrpush(number -> natural, x);
@@ -60,6 +60,7 @@ int main(void)
                 // 닫는 괄호가 입력으로 들어오면 여는 괄호가 나올 때 까지 연산자 꺼내기
                 else if (x == ')') {  
                         printf("%c",x);
+                        pre = x; //이전 문자(pre) 업데이트
                         while (operator -> next != NULL) {
                                 char oper = stackpop(operator);
                                 if (oper == '(')
@@ -67,29 +68,9 @@ int main(void)
                                 
                                 rear_number = pop(numbers);
                                 if (oper == '-') {
-                                        if (isEmpty(numbers)) {
-                                                rear_number -> data = (rear_number -> data == '+') ? '-' : '+';
-                                                push(numbers,rear_number);
-                                        }
-                                        else {
-                                                front_number = pop(numbers);
-                                                if (front_number -> data =='-' && rear_number -> data =='+') {
-                                                        result_number = plus(front_number,rear_number);
-                                                        result_number -> data = '-';
-                                                        push(numbers,result_number);
-                                                }
-                                                else if(front_number -> data == '+' && rear_number -> data =='-') {
-                                                        result_number = plus(front_number,rear_number);
-                                                        result_number -> data = '+';
-                                                        push(numbers,result_number);
-                                                }
-                                                else {
-                                                        result_number = minus(front_number,rear_number);
-                                                        push(numbers,result_number);
-                                                }
-                                        }              
+                                        rear_number -> data = '-';
+                                        push(numbers,rear_number);
                                 }
-
                                 else if (oper == '+') {
                                         if (isEmpty(numbers))
                                                 push(numbers,rear_number);
@@ -112,60 +93,46 @@ int main(void)
                                         push(numbers,result_number);
                                 }
                         } 
-                        gwalho-=1;
+                        if (operator->next != NULL && operator -> next -> data == '-')
+                                numbers -> next -> data -> data = stackpop(operator);
+                        else if (operator->next != NULL && operator -> next -> data == '*') {
+                                stackpop(operator);
+                                push(numbers,multiplication(pop(numbers),pop(numbers)));
+                        }
                 }
 
-                // 여는 괄호가 들어오면 gwalho에 값을 1늘림
+                // 여는 괄호가 들어오면 무조건 푸쉬
                 else if (x == '(') { 
                         printf("%c",x);
                         //곱셈 기호 생략
                         //숫자 * 괄호 or 괄호 * 괄호 or 음수 부호 * 괄호
-                        if (('0' <= pre && pre <= '9') || pre == ')' || pre == '-') {
+                        if (('0' <= pre && pre <= '9') || pre == ')') 
                                 stackpush(operator, '*');
-                        }
-                        stackpush(operator, x);
-                        start = 1;
-                        gwalho+=1;
+                        pre = x;
+                        stackpush(operator,x);
                 }
 
-                // 여는 괄호가 주어지면 계속 연산자를 넣어야하기 때문에 l이 0이 아니고 스택의 마지막에 저장된 연산자보다 넣을 연산자가 크면 push
-                else if (gwalho != 0 || operator -> next == NULL || priority(operator, x) == 2) {
+                // 연산자 순위가 밀리거나 연산자 stack이 비어있으면 푸쉬
+                else if (operator -> next == NULL || priority(operator, x) == 2) {
                         printf("%c",x);
-                        if (x == '-' && start == 1)
-                                minus = 1;
-                        start = 0
-                        stackpush(operator, x);
+                        if (x == '-')
+                                stackpush(operator,'+');
+                        stackpush(operator,x);
+                        pre = x; //이전 문자(pre) 업데이트
+                        
                 }
 
                 // 넣을 연산자랑 스택에 연산자의 우선순위가 작거나 같으면 가능한 만큼 pop하고 연산자 push
-                else if (x=='+' || x == '-' || x == '*' || x == '/'){
+                else if (x=='+'  || x == '*' || x == '/' || x=='-'){
+                        pre = x; //이전 문자(pre) 업데이트
                         printf("%c",x);
                         while (operator -> next != NULL) {
                                 char oper = stackpop(operator);
                                 rear_number = pop(numbers);
-                                
                                 if (oper == '-') {
-                                        if (isEmpty(numbers)) {
-                                                rear_number -> data = (rear_number -> data == '+') ? '-' : '+';
-                                                push(numbers,rear_number);
-                                        }
-                                        else {
-                                                front_number = pop(numbers);
-                                                if (front_number -> data =='-' && rear_number -> data =='+') {
-                                                        result_number = plus(front_number,rear_number);
-                                                        result_number -> data = '-';
-                                                        push(numbers,result_number);
-                                                }
-                                                else if(front_number -> data == '+' && rear_number -> data =='-') {
-                                                        result_number = plus(front_number,rear_number);
-                                                        result_number -> data = '+';
-                                                        push(numbers,result_number);
-                                                }
-                                                else {
-                                                        result_number = minus(front_number,rear_number);
-                                                        push(numbers,result_number);
-                                                }
-                                        }              
+                                        rear_number->data = '-';
+                                        push(numbers,front_number);
+                                        push(numbers,rear_number);
                                 }
                                 else if (oper == '+') {
                                         if (isEmpty(numbers))
@@ -191,6 +158,8 @@ int main(void)
                                 if (operator -> next == NULL || priority(operator, oper) == 2)
                                         break;
                         }
+                        if (x == '-')
+                                stackpush(operator,'+');
                         stackpush(operator, x);
                 }
                 
@@ -200,20 +169,9 @@ int main(void)
                 rear_number = pop(numbers);
                 front_number = pop(numbers);
                 if (oper == '-') {
-                        if (front_number -> data =='-' && rear_number -> data =='+') {
-                                result_number = plus(front_number,rear_number);
-                                result_number -> data = '-';
-                                push(numbers,result_number);
-                        }
-                        else if(front_number -> data == '+' && rear_number -> data =='-') {
-                                result_number = plus(front_number,rear_number);
-                                result_number -> data = '+';
-                                push(numbers,result_number);
-                        }
-                        else {
-                                result_number = minus(front_number,rear_number);
-                                push(numbers,result_number);
-                        }                 
+                        rear_number->data = '-';
+                        push(numbers,front_number);
+                        push(numbers,rear_number);
                 }
                 else if (oper == '+') {
                         if (front_number -> data == '+' && rear_number -> data == '+') {
@@ -252,5 +210,7 @@ int main(void)
                 }
         }
         printf("\n");
+        free(operator);
+        free(numbers);
         return 0;
 }
